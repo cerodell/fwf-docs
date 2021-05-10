5d078bc99afbbd862a26973b0ba352be8ad31e42
Chris Rodell <crodell@eoas.ubc.ca>
2021-05-09 19:35:26 -0700

    :rocket: new theme and logo

--- source/conf.py
+++ source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 
-# project = 'Fire Weather Forecast Model'
+project = ''
 copyright = '2020, Christopher Rodell'
 author = 'Christopher Rodell'
 
